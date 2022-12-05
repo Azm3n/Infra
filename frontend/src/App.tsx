0f1fe67221bfe095c86fa1ae32987b3a2aaa381e
Klaudiusz Dąbrowski <93883022+Azm3n@users.noreply.github.com>
2022-12-05 11:18:10 +0100

    Basic login and register flow (#4)
    * Added: /login, /register endpoints
    
    * Added: LoginPage, RegisterPage, MainPage templates
    
    * Fixes
    
    Co-authored-by: Klaudiusz Dabrowski <klaudiuszdabrowski@arcsoftware.it>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,36 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { LoginPage, RegisterPage } from "./components";
+import { MainPage, LoginOrRegisterPage } from "./components";
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/login"
+            element={
+              <LoginOrRegisterPage
+                endpoint="login"
+                redirectEndpoint="register"
+                submitButtonContent="Login"
+                question="Don't you have account?"
+                redirectContent="Sign up"
+              />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <LoginOrRegisterPage
+                endpoint="register"
+                redirectEndpoint="login"
+                submitButtonContent="Sign up"
+                question="Already have an account."
+                redirectContent="Login"
+              />
+            }
+          />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Router>
     </>
