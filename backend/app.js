cfc349ef1dc11cb116e1f6dfe4b05db5774e0321
Klaudiusz Dabrowski <klaudiuszdabrowski@arcsoftware.it>
2023-01-12 16:57:13 +0100

    Supabase connection

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
 import express, { json, urlencoded } from "express";
 import morgan from "morgan";
 import cors from "cors";
-
-import cors from "cors";
-
-import connection from "./db-connection.js";
+import { v4 as uuidv4 } from 'uuid';
+import { client } from './db-connection.js';
 
 const PORT = 4000;
 const app = express();
@@ -15,20 +13,22 @@ app.use(urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server is up!");
 });
 
 app.post("/login", async (req, res) => {
   const { nickname, password } = req.body;
 
   try {
-    await connection.connect();
-    const qr = await connection.query(
-      `SELECT username, password FROM users WHERE username='${nickname}' AND password='${password}'`
-    );
-    await connection.close();
-    if (!qr.rows.length) res.sendStatus(403);
-    else res.sendStatus(200);
+    const response = await client.from('users').select('name, password').eq('name', nickname).eq('password', password);
+    console.log(response.data);
+    // const qr = await connection.query(
+    //   `SELECT username, password FROM users WHERE username='${nickname}' AND password='${password}'`
+    // );
+    // await connection.close();
+    // if (!qr.rows.length) res.sendStatus(403);
+    // else res.sendStatus(200);
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
@@ -38,13 +38,16 @@ app.post("/login", async (req, res) => {
 app.post("/register", async (req, res) => {
   const { nickname, password } = req.body;
   try {
-    await connection.connect();
-    const qr = await connection.query(
-      `INSERT INTO users ( username, password ) SELECT '${nickname}', '${password}' WHERE NOT EXISTS ( select 1 from users where username='${nickname}' )`
-    );
-    await connection.close();
-    if (!qr.rowsAffected) res.sendStatus(400);
-    else res.sendStatus(200);
+    const response = await client.from('users').insert({ id: uuidv4(), name: nickname, password });
+    console.log(response);
+    res.sendStatus(200);
+    // await connection.connect();
+    // const qr = await connection.query(
+    //   `INSERT INTO users ( username, password ) SELECT '${nickname}', '${password}' WHERE NOT EXISTS ( select 1 from users where username='${nickname}' )`
+    // );
+    // await connection.close();
+    // if (!qr.rowsAffected) res.sendStatus(400);
+    // else res.sendStatus(200);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
--- backend/db-connection.js
+++ backend/db-connection.js
@@ -1,11 +1,6 @@
-import { Connection } from "postgresql-client";
+import { createClient } from '@supabase/supabase-js'
 
-const connection = new Connection({
-  host: "db",
-  port: 5432,
-  user: "docker",
-  password: "123456",
-  database: "docker",
-});
+const supabaseUrl = 'https://tyinxeyuiualdebpkbuc.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5aW54ZXl1aXVhbGRlYnBrYnVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM0MzczNTYsImV4cCI6MTk4OTAxMzM1Nn0.zggESmiz6NlOuoF0SJBcVn1mHCVagYAHbHb-NTWBdf0'
 
-export default connection;
+export const client = createClient(supabaseUrl, supabaseKey)
